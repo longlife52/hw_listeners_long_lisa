4efdcb67dc3e791d504fdf1ecdaabd09233caac7
Lisa Long <garrisonridge@gmail.com>
2017-03-16 17:34:07 -0400

    Add js event listener shell for mouseover changeCirlce

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//changeCircle mouseover
+//listen for  mouseover
+//change color from red to blue
+var redToBlue = document.getElementById('changeCircle');
+document.addEventListener ('mouseover', function () {
+
+})
