729398c4b9e199ee237cee8e3f58eae8f2b208b0
Lisa Long <garrisonridge@gmail.com>
2017-03-16 15:48:59 -0400

    Add event listener

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 //form
 var firstNameEntry = document.getElementById('firstName');
 var lastNameEntry = document.getElementById('lastName');
-var emailEntry = document.getElementbyId('email');
-var commentsEntry = document.getElementbyId('comments');
-document.addEventListener('submit', function() {
-
+var emailEntry = document.getElementById('email');
+var commentsEntry = document.getElementById('comments');
+document.addEventListener('submit', function (noSend) {
+    noSend.preventDefault();
+    console.log('The firstName is:' + firstNameEntry.value);
+    console.log('The lastName is: ' + lastNameEntry.value);
+    console.log('The email is: ' + emailEntry.value);
+    console.log('The message is: ' + commentsEntry.value);
 });
