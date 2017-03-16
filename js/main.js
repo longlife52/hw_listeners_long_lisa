1b58b644084d2606edb3f50523c4267dc307e189
Lisa Long <garrisonridge@gmail.com>
2017-03-16 19:33:01 -0400

    Change scroll event to double click in js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ var changeCircle = document.getElementById('changeCircle');
 changeCircle.addEventListener('mouseover', function () {
     changeCircle.style.backgroundColor = '#caebf2';
 });
+
+//doubleClickEffect
+//listen for a scroll
+//when scroll, change rectangle from solid to transparent
+var doubleClickEffect = document.getElementById('doubleClickEffect');
+
+doubleClickEffect.addEventListener('dblclick', function () {
+    doubleClickEffect.style.background = 'rgba(169, 169, 169, 0.2)';
+});
