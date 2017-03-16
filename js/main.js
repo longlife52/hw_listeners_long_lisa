9cb57d4846aa1fd43d58b20e60a790282a3b9587
Lisa Long <garrisonridge@gmail.com>
2017-03-15 20:33:19 -0400

    Add js for h1 click listener

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//h1click listener
+
+var headerClick = document.getElementById('h1click');
+var clicks = 0;
+
+headerClick.addEventListener('click', function () {
+    var newParagraph = document.createElement('newParagraph');
+    console.log(++clicks);
+    newParagraph.innerHTML = '<p>This is click number</p>' + clicks;
+    document.getElementById('h1click').appendChild(newParagraph);
+});
