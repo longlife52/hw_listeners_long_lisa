d581bc363e09c7be789d321e89e802728985c867
Lisa Long <garrisonridge@gmail.com>
2017-03-15 20:49:45 -0400

    Adjust formatting

--- js/main.js
+++ js/main.js
@@ -6,6 +6,6 @@ var clicks = 0;
 headerClick.addEventListener('click', function () {
     var newParagraph = document.createElement('newParagraph');
     console.log(++clicks);
-    newParagraph.innerHTML = '<p>This is click number</p>' + clicks;
+    newParagraph.innerHTML = '<p>This is click number </p>' + clicks;
     document.getElementById('h1click').appendChild(newParagraph);
 });
